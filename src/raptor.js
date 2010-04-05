1d9d984fe2ae9f71081e996719318d23df58edf6
dgalarza <galarza.d@gmail.com>
2010-04-04 22:26:15 -0400

    Updated raptor.ready for raptor.module.js file naming convention, added raptor.extend and raptor.ready

--- src/raptor.js
+++ src/raptor.js
@@ -38,7 +38,7 @@
 				_loadedModules: [],
 				_scriptReady: [], // temp fix for opera using both onload and onreadystatechange
 				// Array of modules loaded as part of raptor's core
-				_baseModules: []
+				_baseModules: ['util']
 			}
 		};
 	}
@@ -62,8 +62,6 @@
 		return oSelf;
 	}
 	
-	
-
 	/* Module and dependency loading mechanism
 	** 
 	** Usage Examples:
@@ -76,21 +74,32 @@
 			callback: null
 		}, options);
 		
-		
 		// modules can be either a string or array
 		if(typeof(modules) == "string") {
 			_load(modules);
 		} else if(modules.length) {
+			// Hold an internal queue status of what we're requiring
+			var _internalQueue = new Array();
 			for(var i=0; i<modules.length; i++) {
 				_load(modules[i]);
 			}
 		}
 		
-		function _load(mod) {
+		function _queueHandler (mod) {
+			_internalQueue.push(mod);
+			raptor.config._scriptReady.push(mod);
+		};
+		
+		/**
+		* Internal module loader
+		*
+		* @param{String} Module we're loading
+		*/
+		function _load (mod) {
 			if(!raptor.config._loadedModules[mod]) {
 				
 				var script = document.createElement("script");
-				script.src = raptor.config._moduleURI + mod.replace(/\./, "/") + ".js";
+				script.src = raptor.config._moduleURI + mod + ".js";
 				script.type = "text/javascript";
 				document.getElementsByTagName("head")[0].appendChild(script);
 				
@@ -99,11 +108,112 @@
 					if(raptor.config._scriptReady.indexOf(mod) > -1) {
 						return true;
 					} else {
-						raptor.config._scriptReady.push(mod);
+						//raptor.config._scriptReady.push(mod);
+						_queueHandler(mod);
 					}
 				}
 			}
-		}
+		};
 	}
+	
+	/**
+	* Extend an object's properties, recursively
+	* handling an array of objects
+	*
+	* @param {Object} Target object to extend into
+	* @param {Object|Array} 
+	*/
+	raptor.extend = function(target, src) {
+		
+		// Extend the target by all the src
+		if(src.length) {
+			var _length = src.length;
+			for(var i=0; i<_length; i++) {
+				target = raptor.extend(target, src[i]);
+			}
+		}
+		// Extend the source properties into the target
+		else {
+			for(var name in src) {
+				target[name] = src[name];
+			}
+		}
+				
+		return target;
+	}
+	
+	/**
+	* Document ready state handler for raptor
+	*/
+	var _ready = {
+		
+		// Hold the ready queue
+		_readyQueue : [],
+		
+		// Hold the ready state
+		_isReady : false,
+		
+		/**
+		* Handle appending a callback to the ready queue
+		*/
+		append : function (fn) {
+			// Handle listeners
+			_ready.handleReady();
+			
+			// If the document is already ready, just execute the callback
+			if(_ready._isReady) {
+				fn();
+			}
+			// Otherwise push to the queue
+			else {
+				_ready._readyQueue.push(fn);
+			}
+			
+		},
+		
+		/**
+		* Handle the ready queue
+		*/
+		ready : function () {
+			if(_ready._readyQueue.length > 0) {
+				var _length = _ready._readyQueue.length;
+				for(var i=0; i<_length; i++) {
+					_ready._readyQueue[i]();
+				}
+			}
+		},
+		
+		handleReady : function () {
+			
+			if ( document.readyState === "complete" ) {
+				_ready._isReady = true;
+				return _ready.ready();
+			}
+			
+			/**
+			* Attach event handlers for the ready state of the page
+			*/
+			if ( document.addEventListener ) {
+				//document.addEventListener( "DOMContentLoaded", _ready.ready, false );
+				window.addEventListener( "load", _ready.ready, false );
+			}
+			else if( document.attachEvent) {
+				window.attachEvent( "onload", _ready.ready );
+			}
+		}
+	};
+	
+	// Extend the raptor object for ready
+	raptor.extend(raptor, {
+		'ready' : _ready.append
+	});
+	
+	// Set up base modules found in the config
+	raptor.ready(function () {
+		var baseModules = raptor.config._baseModules;
+		if(baseModules.length > 0) {
+			raptor.require(baseModules);
+		}
+	});
 	
 })();
