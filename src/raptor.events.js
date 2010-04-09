9f4195c6c030449addd73648060aee617b94eb92
bbarr <bbarr1384@gmail.com>
2010-04-09 17:43:09 -0400

    raptor.events! yay!

--- src/raptor.events.js
+++ src/raptor.events.js
@@ -6,31 +6,6 @@ raptor.events = (function() {
 	
 	var _events = {};
 	
-	var util = {
-		/**
-		 * Tests for data type by constructor name
-		 * 
-		 * @param {Array|String} types
-		 * @param {Array|Boolean|Date|Math|Number|String|RegExp|Object|HTMLElement} data
-		 */
-		type : function(types, data) {
-			var match = false;
-			var test = function(type) {
-				switch(type) {
-					case 'Object':
-						if (typeof data === 'object' && data.length == undefined && data != null) match = true;
-					case 'HTMLElement':
-						if (data.tagName) match = true;
-					default:
-						if (data.constructor && data.constructor.toString().indexOf(type) !== -1) match = true;		
-				}
-			}
-			if (typeof types === 'string') test(types);
-			else for (var i = 0; i < types.length && !match; i++) test(types[i]);
-			return match;
-		}
-	}
-	
 	return {
 		
 		/**
@@ -41,7 +16,7 @@ raptor.events = (function() {
 		 */
 		'add' : function(target, type, cb) {
 			var type = 'on' + type;
-
+			
 			if (typeof _events[type] === 'undefined') _events[type] = [{'target' : target, 'callbacks' : [cb]}];
 			else {
 				
@@ -65,34 +40,33 @@ raptor.events = (function() {
 		/**
 		 * Fires events
 		 * 
-		 * @param {String} event
-		 * @param {Object} data
+		 * @param {Object} event
 		 */
-		'fire' : function(event, data) {
-			data = data || {};
-			event = event || window.event
+		'fire' : function(event) {
+
+			event = event || window.event;
+			var type = 'on' + event.type;
 			
-			var type = event.type || event;
-			type = 'on' + type;
+			var targets = [];
+			if (event.targets || event.target) {
+				targets = (event.targets) ? event.targets : [event.target];	
+			}
+			
+			var fireEventSet = function(cbs) {
+				for (var i = 0; i < cbs.length; i++) cbs[i](event)
+			}
 			
 			if (typeof _events[type] !== 'undefined') {
-				var events = _events[type]
+				var events = _events[type];
 				
-				var _fire = function(_event) {
-					if (util.type('HTMLElement', this)) {
-						return function(_event) {
-							if (_event.target == this) {
-								for (var cb in _event.callbacks) _event.callbacks[cb](data);	
-							}	
+				for (var x = 0; x < events.length; x++) {
+					
+					if (targets.length > 0) {
+						for (var i = 0; i < targets.length; i++) {
+							if (events[x].target == targets[i]) fireEventSet(events[x].callbacks); 
 						}
 					}
-					else return function(_event) {
-						for (var cb in _event.callbacks) _event.callbacks[cb](data);
-					}
-				}();
-				
-				for (var e in events) {
-					_fire(events[e]);
+					else fireEventSet(events[x].callbacks);
 				}
 			}
 		}
