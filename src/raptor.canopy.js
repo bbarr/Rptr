dab7d6213579147bc3844d475f95e75a763c7ef5
unknown <big-red@.(none)>
2010-05-30 18:41:51 -0400

    Now in basic working order.  3 flavors, obtrusive, unobtrusive and tooltip

--- src/raptor.canopy.js
+++ src/raptor.canopy.js
@@ -1,14 +1,15 @@
-/**
- * 
- */
-
 raptor.canopy = (function() {
 	
 	var _overlays = {};
 	
 	var _html = {
 		'templates' : {
-			'standard' : raptor.pack.birth('div', {'class' : 'overlay standard-overlay hide'}, raptor.pack.birth('a', {'href' : '#', 'class' : 'close-overlay'}))
+			'standard' : raptor.pack.birth('div', {'class' : 'overlay standard-overlay hide'}, 
+				[
+					raptor.pack.birth('a', {'href' : '#', 'class' : 'close-overlay'}, 'close'),
+					raptor.pack.birth('div', {'class' : 'content loading'}, 'loading..')
+				]
+			)
 		}
 	};
 	
@@ -29,47 +30,98 @@ raptor.canopy = (function() {
 		
 		'buildFrame' : function(overlay) {
 			overlay.el = (overlay.template) ? _html.templates[overlay.template].cloneNode(true) : _html.templates.standard.cloneNode(true);
-			raptor.pack.append(overlay.el, document.body);
-		},
+			overlay.el.setAttribute('id', overlay.id);
+			
+			raptor.pack.append(overlay.el, document.body, true);
+		}
 	}
 	
 	var Overlay = function(config) {
+		this.id = config.id;
 		this.type = config.type || 'unobtrusive';
-		this.el = document.getElementById(config.id);
-		this.prepared = (this.preCall) ? false : true;
+		this.el = document.getElementById(this.id);
+		
+		// if overlay doens't exist in the document, generate from collection of templates
+		if (!this.el) _util.buildFrame(this);
+
+		if (config.callback) this.callback = config.callback;
 		this.cache = config.cache || true;
 	}
 	
 	Overlay.prototype = {
+		
 		'show' : function(e) {
-			e = e || {};
 			
-			if (!this.el) this.buildFrame();
-			if (!this.prepared) this.preCall(this);
+			// if element exists and is showing, don't try show/build it again
+			if (this.el && !raptor.pack.hasClass('hide', this.el)) return false;
+			
+			// preserve event if there is one
+			this.triggerEvent = e || {};
+			
+			// show backdrop for obtrusive overlays
 			if (this.type === 'obtrusive') _util.toggleObtrusive();
 			
-			this.setPosition(e);
 			raptor.pack.removeClass('hide', this.el);
-		},
-		'hide' : function() {
-		
-			raptor.pack.addClass('hide', this.el);
 			
-			if (this.type === 'obtrusive') this.toggleObtrusive();
 			
-			if (!this.cache) {
-				this.prepared = false;
+			// position it dynamically based on content size, or click location for tooltips
+			this.position();
+			
+			// if there is a callback, execute it, so it can populate the overlay or whatever else it wants to do
+			if (this.callback) {
+				this.callback(this);
+				
+				// finally, if cache is set to true, destroy the callback as it won't be used again
+				if (this.cache) this.callback = null; 
 			}
 		},
-		'setPosition' : function(e) {
+		
+		'hide' : function() {
+			raptor.pack.addClass('hide', this.el);
+			if (this.type === 'obtrusive') _util.toggleObtrusive();
+		},
+		
+		'position' : function() {
+			
+			var el = this.el;
+			var event = this.triggerEvent;
+			var style = {};
+			var width = el.scrollWidth;
+			var height = el.scrollHeight;
+			var clientHeight = document.documentElement.clientHeight;
+			var clientWidth = document.documentElement.clientWidth;
+
 			if (this.type === 'tooltip') {
-				
+				if (event.x && event.y) {
+					style.left = event.x + 'px';
+					style.top = event.y + 'px';
+				}
 			}
 			else {
+				style.left = '50%';
+				style['margin-left'] = '-' + Math.floor(width / 2) + 'px';
 				
+				if (clientHeight > height) {
+					style.top = ((clientHeight - height) / 2) + 'px';
+				}
+				else {
+					style.top = '0px';
+				}
 			}
+
+			raptor.pack.setStyle(style, el);
 		}
 	}
+	
+	var init = function() {
+		raptor.events.add('.close-overlay', 'click', function(e) {
+			e.preventDefault();
+			var overlayID = e.target.parentNode.getAttribute('id');
+			_overlays[overlayID].hide();
+		});
+	}
+	
+	raptor.ready(init);
 	
 	return {
 		init : function(config) {
