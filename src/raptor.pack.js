e7edd46e49b976434d65828d26a2ed54d5c08984
bbarr <bbarr1384@gmail.com>
2010-06-03 17:23:26 -0400

    corrected call to setStyle, which is now in pack.js

--- src/raptor.pack.js
+++ src/raptor.pack.js
@@ -80,7 +80,7 @@ raptor.pack = (function() {
 			if (attrs == '') attrs = {};
 			for (var attr in attrs) {
 				
-				if (attr == 'style') raptor.util.applyStyle(attrs[attr], el);
+				if (attr == 'style') this.setStyle(attrs[attr], el);
 				
 				// Properly handle classes attributes
 				else if( attr === 'class') el.className = attrs[attr];
