337ab04cf6da17f78bafa8e8ffb8a5eb9271b599
dgalarza <galarza.d@gmail.com>
2010-10-03 14:13:13 -0400

    Major overhaul to animation library providing internal stack for managing timeouts

--- src/raptor.animate.js
+++ src/raptor.animate.js
@@ -1,7 +1,7 @@
 (function() {
 	
 	// private
-	var _util;
+	var _util, _animation_stack;
 		
 	// public
 	var api;
@@ -19,12 +19,13 @@
 		* @param {Integer} Rate to change value
 		* @param {Integer} Speed to animate
 		* @param {Boolean} Pixel based animation?
+		* @param {String} Interval name for this animation
 		*/
-		animate : function (el, prop, start, end, each, speed, pixel) {
+		animate : function (el, prop, start, end, each, speed, pixel, interval) {
 			
 			// Setup increment and completion checking methods up front
 			var _increment, _complete;
-			
+						
 			if (start < end) {
 				_increment = function () { return start + each; };
 				_complete = function () { return start <= end; };
@@ -34,6 +35,11 @@
 				_complete = function () { return start >= end; };
 			}
 			
+			// Get a unique id for this aniamtion
+			_animation_stack._auid++;
+			
+			var _id = _animation_stack._auid;
+			
 			var _animate = function () {
 				start = _increment();
 				
@@ -42,12 +48,63 @@
 				}
 				else {
 					el.style[prop] = (pixel) ? end + 'px' : end;
-					clearInterval(_interval);
+					api.dequeue_animation(interval, _id);
 				}
 				
 			};
 			
-			var _interval = setInterval(_animate, speed);
+			api.queue_animation(interval, speed, _animate, _id);
+		}
+		
+	};
+	
+	/**
+	* Internal animation stack to efficiently make use of timeouts on a page
+	*/
+	_animation_stack = {
+		
+		// Internal animation unique identifiers
+		_auid : 0,
+		
+		// Collection of intervals to work with on the stack
+		intervals : {},
+		
+		// Collection of interval callbacks to execute on interval iteration
+		interval_callbacks : {},
+		
+		/**
+		* Attempts to create an interval with the name provided in the collection
+		* if one does not already exist
+		* 
+		* @param {String} The name of the interval
+		* @param {Integer} Time delay for interval
+		*/
+		create_interval : function (name, interval) {
+			if (this.intervals[name]) return;
+			
+			this.intervals[name] = setInterval(function () {
+				var queue = _animation_stack.interval_callbacks[name];
+				if (queue && !raptor.obj_empty(queue)) {
+					for (var fn in queue) queue[fn].call();
+				}
+				else {
+					clearInterval(_animation_stack.intervals[name]);
+					delete _animation_stack.intervals[name];
+				}
+				
+			}, interval);
+		},
+		
+		/**
+		* Pushes a function to the queue for a specified named interval
+		*
+		* @param {String} Interval to work with
+		* @param {Function} The callback to add to the queue
+		* @param {String} Identifier for the function in the queue
+		*/
+		push_to_queue : function (interval, fn, id) {
+			if (!this.interval_callbacks[interval]) this.interval_callbacks[interval] = {};
+			this.interval_callbacks[interval][id] = fn;
 		}
 		
 	};
@@ -130,16 +187,44 @@
 		* @param {Integer} Rate of change
 		* @param {Integer} Speed to execute at
 		* @param {String} Direction for animation (CSS property) (top, right, bottom, left)
+		* @param {String} Specify an interval to use for this aniamtion, otherwise, raptor will group similar speeds
 		*/
-		slide : function (el, start, end, each, speed, prop) {
+		slide : function (el, start, end, each, speed, prop, interval) {
 			
 			each = each || 0.10;
 			speed = speed || 10;
 			prop = prop || 'left'
+			interval = interval || speed;
 			
-			_util.animate(el, prop, start, end, each, speed, true);
-		}
+			_util.animate(el, prop, start, end, each, speed, true, interval);
+		},
+				
+		/**
+		* Add a callback to an interval for animation purposes
+		* Allows user to define a named interval to manually group together
+		* intervals as desired into a single stack.
+		*
+		* A user's callback is responsible for removing itself from the queue
+		*
+		* @param {String} Interval name
+		* @param {Integer} Time delay for interval
+		* @param {Function} Function to add to the queue
+		* @param {String} Identifier for the callback in the queue
+		*/ 
+		queue_animation : function (interval, delay, fn, id) {
+			_animation_stack.push_to_queue(interval, fn, id);
+			_animation_stack.create_interval(interval, delay);
+		},
 		
+		/**
+		* Removes a method from specified interval's queue
+		*
+		* @param {String} Interval name
+		* @param {String|Integer} Id of method to remove
+		*/
+		dequeue_animation : function (interval, id) {
+			delete _animation_stack.interval_callbacks[interval][id];
+		}
 	}
 	
 	if (raptor) raptor.extend(api);
