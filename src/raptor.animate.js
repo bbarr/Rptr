daad5c52ef9d2236fd0bbae8c7869c68d5577148
dgalarza <galarza.d@gmail.com>
2010-10-02 21:55:18 -0400

    Created reusable private animate routine

--- src/raptor.animate.js
+++ src/raptor.animate.js
@@ -1,6 +1,60 @@
 (function() {
 	
+	// private
+	var _util;
+		
+	// public
 	var api;
+	
+	_util = {
+		
+		/**
+		* Reuseable animation function for animatin
+		* CSS properties
+		*
+		* @param {HTMLElement} Element to animate
+		* @param {String} Property to animate
+		* @param {Integer} Start Value
+		* @param {Integer} End Value
+		* @param {Integer} Rate to change value
+		* @param {Integer} Speed to animate
+		* @param {Boolean} Pixel based animation?
+		*/
+		animate : function (el, prop, start, end, each, speed, pixel) {
+		
+			// Determine the method to update the animation property
+			var _increment = (start < end) ? function () { return start + each; } : function () { return start - each; };
+			var _complete = (start < end) ? function () { return start <= end; } : function () { return start >= end; }
+			
+			// Setup increment and completion checking methods up front
+			var _increment, _complete;
+			
+			if (start < end) {
+				_increment = function () { return start + each; };
+				_complete = function () { return start <= end; };
+			}
+			else {
+				_increment = function () { return start - each; };
+				_complete = function () { return start >= end; };
+			}
+			
+			var _animate = function () {
+				start = _increment();
+				
+				if ( _complete() ) {
+					el.style[prop] = start + 'px';
+				}
+				else {
+					el.style[prop] = end + 'px';
+					clearInterval(_interval);
+				}
+				
+			};
+			
+			var _interval = setInterval(_animate, speed);
+		}
+		
+	};
 	
 	api = {
 		
@@ -70,6 +124,24 @@
 			}
 			
 			var interval = setInterval(_increment, speed);
+		},
+		
+		/**
+		* Slide animation
+		* @param {HTMLElement} The element to slide
+		* @param {Integer} The starting position
+		* @param {Integer} End position for slide
+		* @param {Integer} Rate of change
+		* @param {Integer} Speed to execute at
+		* @param {String} Direction for animation (CSS property) (top, right, bottom, left)
+		*/
+		slide : function (el, start, end, each, speed, prop) {
+			
+			each = each || 0.10;
+			speed = speed || 10;
+			prop = prop || 'left'
+			
+			_util.animate(el, prop, start, end, each, speed, true);
 		}
 		
 	}
