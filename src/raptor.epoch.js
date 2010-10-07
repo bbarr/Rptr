6d694c69ca28b73145083346316358d139d1b10c
bbarr <bbarr1384@gmail.com>
2010-10-07 15:46:54 -0400

    with additional api, util methods

--- src/raptor.epoch.js
+++ src/raptor.epoch.js
@@ -12,8 +12,35 @@
 	var _months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var _short_months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	
-	util = {
-	
+	_util = {
+		
+		days_in_month : function(month, year) {
+			month = month - 1;
+			var m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+			if (month != 1) return m[month];
+			if (year % 4 != 0) return m[1];
+			if (year % 100 == 0 && year%400 != 0) return m[1];
+			return m[1] + 1;
+		},
+		
+		parse_string : function(string) {
+			var parts = string.split((string.indexOf('-') > -1) ? '-' : '/');
+			month = parts[0];
+			
+			if (parts.length === 2) year = parts[1];
+			else {
+				day = parts[1];
+				year = parts[2];
+			}
+			
+			if (typeof day === 'undefined') day = 1;
+			
+			return {
+				day : parseInt(day),
+				month : parseInt(month),
+				year : parseInt(year)
+			}
+		}
 	};
 	
 	api = {
@@ -27,6 +54,19 @@
 			// If a string was passed in let's turn it into a usable index
 			if (typeof month === 'string') month = parseInt(month, 10) - 1;
 			return _short_months[month];
+		},
+		
+		get_days_in_month : function(month, year) {
+			if (typeof month === 'string') {
+				var date = _util.parse_string(month);
+				month = date.month;
+				year = date.year;
+			}			
+			return _util.days_in_month(month, year);
+		},
+		
+		string_to_date : function(string) {
+			return _util.parse_string(string);
 		}
 		
 	};
